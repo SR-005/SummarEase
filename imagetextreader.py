dc9918f3e850d4dc84819792c6fc842e0ed418e3
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-07-24 22:10:08 +0530

    setup threshold

--- imagetextreader.py
+++ imagetextreader.py
@@ -9,13 +9,16 @@ print("Image Read Successfull")
 Reader=easyocr.Reader(['en'],gpu=True)  #create an instance for reader using easyocr, set gpu to TRUE for faster computation
 readtext=Reader.readtext(image)     #using instance read text from image
 
+threshold=0.25
 #drawing boxes for text's found:
 for i in readtext:   
     print(i)        #print original values
     box,text,score=i    #seperate the raw values
-    topleftbox=tuple(box[0])    #get the coordinates of the topleft point of bounding box, we use tuple cause it's in format [x,y]
-    bottomrightbox=tuple(box[2])    #get the coordinates of the bottomright point of bounding box
-    cv2.rectangle(image,topleftbox,bottomrightbox,(255,0,0),4)  #draw the rectange using extracted coordinates
+
+    if score>threshold:
+        topleftbox=tuple(box[0])    #get the coordinates of the topleft point of bounding box, we use tuple cause it's in format [x,y]
+        bottomrightbox=tuple(box[2])    #get the coordinates of the bottomright point of bounding box
+        cv2.rectangle(image,topleftbox,bottomrightbox,(255,0,0),4)  #draw the rectange using extracted coordinates
 
 plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))  #convert the BGRcoloured image to RGBcoloured
 plt.show() 
