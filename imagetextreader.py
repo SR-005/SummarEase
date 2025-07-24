7b3eee9a2cfd9fe2cbfc4f90ecbd3bad30b4e4d9
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-07-24 12:21:28 +0530

    drawn bounding box

--- imagetextreader.py
+++ imagetextreader.py
@@ -1,13 +1,21 @@
 import cv2
 import easyocr
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 imagepath="test.jpeg"       #specify image path here
 image=cv2.imread(imagepath)     #read the image using imagepath
 print("Image Read Successfull")
 
 Reader=easyocr.Reader(['en'],gpu=True)  #create an instance for reader using easyocr, set gpu to TRUE for faster computation
-text=Reader.readtext(image)     #using instance read text from image
+readtext=Reader.readtext(image)     #using instance read text from image
 
-for i in text:   
-    print(i)
+#drawing boxes for text's found:
+for i in readtext:   
+    print(i)        #print original values
+    box,text,score=i
+    topleftbox=tuple(box[0])
+    bottomrightbox=tuple(box[2])
+    cv2.rectangle(image,topleftbox,bottomrightbox,(255,0,0),4)
+
+plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+plt.show() 
