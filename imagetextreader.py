32b9991e16e1a6f1737842879403ac182845342b
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-07-24 12:04:41 +0530

    first read complete

--- imagetextreader.py
+++ imagetextreader.py
@@ -2,4 +2,12 @@ import cv2
 import easyocr
 import matplotlib as plt
 
-imgpath="test.jpeg"
+imagepath="test.jpeg"       #specify image path here
+image=cv2.imread(imagepath)     #read the image using imagepath
+print("Image Read Successfull")
+
+Reader=easyocr.Reader(['en'],gpu=True)  #create an instance for reader using easyocr, set gpu to TRUE for faster computation
+text=Reader.readtext(image)     #using instance read text from image
+
+for i in text:   
+    print(i)
