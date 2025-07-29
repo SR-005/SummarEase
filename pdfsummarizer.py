7787ade2dd7e1859d55d02e821a466a0bce99e5b
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-07-29 20:14:15 +0530

    finishing pdf summarzing

--- pdfsummarizer.py
+++ pdfsummarizer.py
@@ -13,3 +13,14 @@ document.close()
 
 #spliting words according to max limit
 formattedtext=textwrap.wrap(text,maxcharacter)
+
+#Summarizer
+summarease = pipeline("summarization", model="facebook/bart-large-cnn")     #setting up summarizer model
+
+appendsummary=[]
+for i in formattedtext:
+    summary = summarease(i,  max_length=130, min_length=30, do_sample=False)[0]['summary_text']  #properties adjustments
+    appendsummary.append(summary)
+
+fullsummary="\n\n".join(appendsummary)
+print(fullsummary)
