43d6f31e09588e6a0cb8ccaccdf378c06c5570e3
Sreeram V Gopal <sreeramvg100@gmail.com>
2025-08-09 12:59:17 +0530

    getting files from forums

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ app=Flask(__name__)
 
 @app.route("/",methods=["GET", "POST"])
 def index():
+    if request.method == ['POST']:
+        filetype=request.form("inputTypeSelector")
+
+        if filetype=="document":
+            docfile=request.files.get("documentFile") #extract the doc file from submitted forum
+
+        elif filetype=="website":
+            url=request.form.get("websiteURL")  #extract the url from submitted forum
+
+        elif filetype=="image":
+            imgfile=request.files.get("imageFile")  #extract the image file from submitted forum
+
     return render_template("index.html")
 
 if __name__=="__main__":
